c6c26a33dfaa64b3295245cdeab1226cfc9cdd94
Adam Jasinski <ajasinski@inishtech.com>
2012-05-09 15:50:42 +0100

    Added license info to source files

--- Sp.Samples.Agent.WpfApplicationWithInstaller/Sp.Samples.Agent.WpfApplication/BusinessLogic/MyAlgorithms.cs
+++ Sp.Samples.Agent.WpfApplicationWithInstaller/Sp.Samples.Agent.WpfApplication/BusinessLogic/MyAlgorithms.cs
@@ -1,4 +1,12 @@
-﻿namespace Sp.Samples.Agent.WpfApplication.BusinessLogic
+﻿/*
+ * Copyright (c) Inish Technology Ventures Limited.  All rights reserved.
+ * 
+ * This code is licensed under the BSD 3-Clause License included with this source
+ * 
+ * ALSO SEE: https://github.com/SoftwarePotential/samples/wiki/License
+ * 
+ */
+namespace Sp.Samples.Agent.WpfApplication.BusinessLogic
 {
 	using Slps.ProtectionAttributes;
 
--- Sp.Samples.Agent.WpfApplicationWithInstaller/Sp.Samples.Agent.WpfApplication/Properties/AssemblyInfo.cs
+++ Sp.Samples.Agent.WpfApplicationWithInstaller/Sp.Samples.Agent.WpfApplication/Properties/AssemblyInfo.cs
@@ -1,4 +1,12 @@
-﻿using System.Reflection;
+﻿/*
+ * Copyright (c) Inish Technology Ventures Limited.  All rights reserved.
+ * 
+ * This code is licensed under the BSD 3-Clause License included with this source
+ * 
+ * ALSO SEE: https://github.com/SoftwarePotential/samples/wiki/License
+ * 
+ */
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows;
 
