8e64b9695a145464a93ad012cee8b005fa682a1a
Johannes Rudolph <jojo.rudolph@gmail.com>
2012-11-09 16:15:30 +0100

    Added Obfuscator Project with Library

---
+++ Sp.Samples.IntegratingObfuscators/Sp.Samples.IntegratingObfuscators.Library/AnswerService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sp.Samples.IntegratingObfuscators.Library
+{
+    public class AnswerService
+    {
+        public string GetAnswer()
+        {
+            return GetAnswerObfuscatable();
+        }
+
+        private static string GetAnswerObfuscatable()
+        {
+            return "42";
+        }
+    }
+}
---
+++ Sp.Samples.IntegratingObfuscators/Sp.Samples.IntegratingObfuscators/Program.cs
@@ -0,0 +1,21 @@
+﻿using Sp.Samples.IntegratingObfuscators.Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sp.Samples.IntegratingObfuscators
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            MainObfuscatable();
+        }
+
+        private static void MainObfuscatable()
+        {
+            var service = new AnswerService();
+            Console.WriteLine( service.GetAnswer() );
+        }
+    }
+}
