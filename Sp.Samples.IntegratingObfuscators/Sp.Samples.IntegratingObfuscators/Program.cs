5c5ea2efa0ed73b6df90cd7a7b05d0235b97afbc
Johannes Rudolph <jojo.rudolph@gmail.com>
2012-11-16 10:30:34 +0100

    Obfuscation Sample verifies itself for successfull obfuscation

--- Sp.Samples.IntegratingObfuscators/Sp.Samples.IntegratingObfuscators/Program.cs
+++ Sp.Samples.IntegratingObfuscators/Sp.Samples.IntegratingObfuscators/Program.cs
@@ -1,7 +1,10 @@
-﻿using Sp.Samples.IntegratingObfuscators.Library;
+﻿using Slps.ProtectionAttributes;
+using Sp.Samples.IntegratingObfuscators.Library;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 
 namespace Sp.Samples.IntegratingObfuscators
 {
@@ -10,12 +13,32 @@ namespace Sp.Samples.IntegratingObfuscators
         static void Main( string[] args )
         {
             MainObfuscatable();
+
+            AssertObfuscated( MethodOf( () => MainObfuscatable() ) );
+            
+            Console.ReadLine();
         }
 
+        /// <summary>
+        /// This method will be protected and an obfuscator should be able to obfuscate it
+        /// </summary>
+        [Feature]
         private static void MainObfuscatable()
         {
             var service = new AnswerService();
-            Console.WriteLine( service.GetAnswer() );
+            Console.WriteLine( "The answer is: " + service.GetAnswer() );
+        }
+
+        public static MethodInfo MethodOf( Expression<Action> expression )
+        {
+            MethodCallExpression body = (MethodCallExpression)expression.Body;
+            return body.Method;
+        }
+
+        private static void AssertObfuscated( MethodInfo method )
+        {
+            if (method.Name == "MainObfuscatable")
+                Console.WriteLine( "MainObfuscatable() was not obfuscated" );
         }
     }
 }
