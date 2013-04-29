ead5de4e050a4278c8980bb2bae54f8f1a9f34d8
Siobhan Connell <connell.siobhan@gmail.com>
2013-04-29 15:36:15 +0100

    Fixed license reference

--- Sp.Samples.Consume/build.ps1
+++ Sp.Samples.Consume/build.ps1
@@ -1,8 +1,7 @@
-# Copyright (c) 2013 Inish Technology Ventures Limited.  All rights reserved.
-#  
+# * Copyright 2013 (c) Inish Technology Ventures Limited.  All rights reserved.
 # This code is licensed under the BSD 3-Clause License included with this source
-# 
-# FOR DETAILS, SEE https://github.com/InishTech/Sp.Api/wiki/License 
+#
+# ALSO SEE: https://github.com/SoftwarePotential/samples/wiki/License
 param(
 	[string] $solution="build.proj",
 	[string] $targets="Build",
--- Sp.Samples.Consume/configure.ps1
+++ Sp.Samples.Consume/configure.ps1
@@ -1,8 +1,12 @@
-# Copyright (c) 2013 Inish Technology Ventures Limited.  All rights reserved.
-#  
+# * Copyright 2013 (c) Inish Technology Ventures Limited.  All rights reserved.
 # This code is licensed under the BSD 3-Clause License included with this source
-# 
-# FOR DETAILS, SEE https://github.com/InishTech/Sp.Api/wiki/License 
+#
+# ALSO SEE: https://github.com/SoftwarePotential/samples/wiki/License
+
+// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+// PARTICULAR PURPOSE.
 param(
 
 	[string] $Username = $(Read-Host -prompt "Software Potential username (account@domain.com)"),
