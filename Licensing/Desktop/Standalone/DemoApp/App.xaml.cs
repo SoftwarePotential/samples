2d27a041d7dc73e2f7d2818d3e69a6db7749f676
Siobhan Connell <connell.siobhan@gmail.com>
2014-01-02 16:44:48 +0000

    Created wpf sample based on SP NuGet packages

---
+++ Licensing/Desktop/Standalone/DemoApp/App.xaml.cs
@@ -0,0 +1,59 @@
+﻿using DemoApp.Activation;
+using Sp.Agent;
+using Sp.Agent.Execution;
+using Sp.Agent.Storage;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace DemoApp
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+
+		// Called once during application startup
+		// (event handler registered in App.xaml)
+		void Application_Startup( object sender, StartupEventArgs e )
+		{
+			try
+			{	// Initialize Sp Agent
+				SpAgent.Configuration.VerifyStoresInitialized();
+			}
+			catch ( WritingStorageInaccessibleException exc )
+			{
+				ShowErrorMessageBox( "Unable to initialize the store in local user storage.\n\n" + exc.Message );
+			}
+		}
+
+		// Global exception handling  
+		// (event handler registered in App.xaml)
+		void Application_DispatcherUnhandledException( object sender, DispatcherUnhandledExceptionEventArgs e )
+		{
+
+			if ( e.Exception is NotLicensedException )
+			{
+				ShowErrorMessageBox( "You don't have a license for this feature" );
+				e.Handled = true;
+
+				// Redirect them to the activation dialog so they can install a valid license
+				var activationDialog = new ActivationDialog { Owner = MainWindow };
+				activationDialog.ShowDialog();
+			}
+			else
+			{
+				//All other exceptions
+				string exceptionMessage = e.Exception != null ? e.Exception.Message : e.ToString();
+				ShowErrorMessageBox( "An error occurred: " + exceptionMessage );
+				e.Handled = false;
+			}
+		}
+
+		void ShowErrorMessageBox( string messageBoxText )
+		{
+			string popupTitle = MainWindow != null ? MainWindow.Title : "Error";
+			MessageBox.Show( messageBoxText, popupTitle, MessageBoxButton.OK, MessageBoxImage.Error );
+		}
+	}
+}
---
+++ Licensing/Desktop/Standalone/DemoApp/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Copyright (c) Inish Technology Ventures Limited.  All rights reserved.
+ * 
+ * This code is licensed under the BSD 3-Clause License included with this source
+ * 
+ * ALSO SEE: https://github.com/SoftwarePotential/samples/blob/master/License.txt
+ * 
+ */
+
+using DemoApp.Activation;
+using DemoApp.BusinessLogic;
+using DemoApp.Licenses;
+using System.Windows;
+
+namespace DemoApp
+{
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void RunFeature1_Click( object sender, RoutedEventArgs e )
+		{
+			MyAlgorithms.AccessFeature1();
+			MessageBox.Show( "Feature 1 accessed successfully", "Success", MessageBoxButton.OK, MessageBoxImage.Information );
+		}
+
+		private void RunFeature2_Click( object sender, RoutedEventArgs e )
+		{
+			MyAlgorithms.AccessFeature2();
+			MessageBox.Show( "Feature 2 accessed successfully", "Success", MessageBoxButton.OK, MessageBoxImage.Information );
+		}
+
+
+		private void ShowInstalledLicensesList_Click( object sender, RoutedEventArgs e )
+		{
+			var licenseListDialog = new LicenseListDialog { Owner = this };
+			licenseListDialog.ShowDialog();
+		}
+
+		void ShowActivationDialog_Click( object sender, RoutedEventArgs e )
+		{
+			var activationDialog = new ActivationDialog { Owner = this };
+			activationDialog.ShowDialog();
+		}
+	}
+}
