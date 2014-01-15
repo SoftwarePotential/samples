fdb220219fe6061c25bcdb1516f24426cc253927
Siobhan Connell <connell.siobhan@gmail.com>
2014-01-15 15:17:50 +0000

    Manual activation server side demo

---
+++ Api/ManualActivation/DemoApp/Models/ManualActivationModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace DemoApp.Models
+{
+	public class ManualActivationModel
+	{
+		[Required, StringLength( 500, MinimumLength = 1 )]
+		[ValidateActivationRequest]
+		public string ActivationRequest { get; set; }
+
+		public Guid FileId { get; set; }
+	}
+
+	class ValidateActivationRequest : ValidationAttribute
+	{
+
+		public override bool IsValid( object value )
+		{
+			try
+			{
+				ActivationRequestHelper.ExtractRequestBlob( (string)value );
+				return true;
+			}
+			catch
+			{
+				return false;
+			}
+		}
+
+		public override string FormatErrorMessage( string name )
+		{
+			return String.Format( CultureInfo.CurrentUICulture, "Invalid manual activation request", name );
+		}
+	}
+}
