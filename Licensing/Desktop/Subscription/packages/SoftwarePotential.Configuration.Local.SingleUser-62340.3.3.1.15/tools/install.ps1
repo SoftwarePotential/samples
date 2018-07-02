e39be2b7f2b477f670522d8ced5c2575b097d240
nboon <nboon@inishtech.com>
2018-07-02 12:37:59 +0100

    Updated Subscription packages

--- Licensing/Desktop/Subscription/packages/SoftwarePotential.Configuration.Local.SingleUser-62340.3.3.1.15/tools/install.ps1
+++
@@ -1,3 +0,0 @@
-﻿# http://stackoverflow.com/a/15403662/11635
-$readmeUrl = "https://support.softwarepotential.com/hc/en-us/articles/115001365849--SingleUser-Configuration-README"
-$DTE.ItemOperations.Navigate($readmeUrl, [EnvDTE.vsNavigateOptions]::vsNavigateOptionsNewWindow)
---
+++ Licensing/Desktop/Subscription/packages/SoftwarePotential.Licensing-Demo_10.1.0.1.3/tools/install.ps1
@@ -0,0 +1,3 @@
+﻿# http://stackoverflow.com/a/15403662/11635
+$readmeUrl = "https://support.softwarepotential.com/hc/en-us/articles/115001358849-Licensing-README"
+$DTE.ItemOperations.Navigate($readmeUrl, [EnvDTE.vsNavigateOptions]::vsNavigateOptionsNewWindow)
