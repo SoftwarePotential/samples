85ef8ee5331719d67ae0ae56e2885cfad6d5c87d
nboon <nboon@inishtech.com>
2018-07-19 14:57:00 +0100

    net core console app: path for license to be installed defaults to desktop with option for alternative location

--- Licensing/NetCore/ConsoleApp/ConsoleApp/InstallLicense.cs
+++ Licensing/NetCore/ConsoleApp/ConsoleApp/InstallLicense.cs
@@ -11,12 +11,13 @@ namespace ConsoleApp
 	{
 		public InstallLicense()
 		{
-			IsCommand( "Install", "Installs a license using the supplied filepath." );
-			HasRequiredOption( "l|license=", "E.g. -l <licenseFilename>", s => _licenseFilename = s );
+			IsCommand( "Install", "Installs a Software Potential license." );
+			HasRequiredOption( "f|filename=", "The filename of the license.", s => _licenseFilename = s );
+			HasOption( "d|directory=", "The path to the directory conainting the license. If not specified, the default is the current user's Desktop.", x => _licenseDir = x );
 		}
 
 		string _licenseFilename = string.Empty;
-		readonly string _licenseDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile ), "Downloads" ); // make cross platform?
+		string _licenseDir = Environment.GetFolderPath(Environment.SpecialFolder.Desktop );
 
 		public override int Run( string[] remainingArguments )
 		{
@@ -26,22 +27,23 @@ namespace ConsoleApp
 
 		void Install()
 		{
+			var path = Path.Combine( _licenseDir, _licenseFilename);
 			try
 			{
-				SpAgent.Product.Stores.Install( File.ReadAllBytes( Path.Combine( _licenseDir, _licenseFilename ) ) );
+				SpAgent.Product.Stores.Install( File.ReadAllBytes( path ) );
 				Console.WriteLine( "Success: The license has been successfully installed." );
 			}
 			catch ( LicenseRevisionException )
 			{
-				Console.Error.WriteLine( "Error: There is a newer version of the license already installed." );
+				Console.WriteLine( "Error: There is a newer version of the license already installed." );
 			}
 			catch ( NonmatchingProductIdException )
 			{
-				Console.Error.WriteLine( $"Error: The given license is not for the product: {SpAgent.Product.ProductName} v{SpAgent.Product.ProductVersion}." );
+				Console.WriteLine( $"Error: The given license is not for the product: {SpAgent.Product.ProductName} v{SpAgent.Product.ProductVersion}." );
 			}
 			catch ( FileNotFoundException )
 			{
-				Console.Error.WriteLine( $"Error: Could not find license file at {_licenseFilename}." );
+				Console.WriteLine( $"Error: Could not find license at {path}." );
 			}
 		}
 	}
